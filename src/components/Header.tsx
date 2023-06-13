462b9546f1bd2e00f3bfc244e6ce569c2001ef0f
owuraku-zenas <zenasawuku@gmail.com>
2023-06-13 18:36:12 +0000

    Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,74 @@
+"use client";
+import { motion } from "framer-motion";
+import React from "react";
+import { SocialIcon } from "react-social-icons";
+
+type Props = {};
+
+const Header = (props: Props) => {
+    return (
+        <header className="sticky top-0 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
+            <motion.div
+                initial={{
+                    x: -500,
+                    opacity: 0,
+                    scale: 0.5,
+                }}
+                animate={{
+                    x: 0,
+                    opacity: 1,
+                    scale: 1,
+                }}
+                transition={{
+                    duration: 0.6,
+                }}
+                className="flex flex-row items-center"
+            >
+                {/* Social Icons */}
+                <SocialIcon
+                    url="https://github.com/owuraku-zenas"
+                    fgColor="gray"
+                    bgColor="transparent"
+                />
+                <SocialIcon
+                    url="https://github.com/owuraku-zenas"
+                    fgColor="gray"
+                    bgColor="transparent"
+                />
+                <SocialIcon
+                    url="https://github.com/owuraku-zenas"
+                    fgColor="gray"
+                    bgColor="transparent"
+                />
+            </motion.div>
+            <motion.div
+                initial={{
+                    x: 500,
+                    opacity: 0,
+                    scale: 0.5,
+                }}
+                animate={{
+                    x: 0,
+                    opacity: 1,
+                    scale: 1,
+                }}
+                transition={{
+                    duration: 0.7,
+                }}
+                className="flex flex-row items-center"
+            >
+                <SocialIcon
+                    className="cursor-pointer"
+                    network="email"
+                    fgColor="gray"
+                    bgColor="transparent"
+                />
+                <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+                    Get In Touch
+                </p>
+            </motion.div>
+        </header>
+    );
+};
+
+export default Header;
