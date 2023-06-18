c9f183b86f3c20d436c5b2bbb03cb6e14bd7c81c
owuraku-zenas <zenasawuku@gmail.com>
2023-06-18 07:46:46 +0000

    frost navbar
    Signed-off-by: owuraku-zenas <zenasawuku@gmail.com>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,33 +1,32 @@
 "use client";
-import { motion } from "framer-motion";
+import {motion} from "framer-motion";
 import React from "react";
-import { SocialIcon } from "react-social-icons";
+import {SocialIcon} from "react-social-icons";
 
-type Props = {};
-
-const Header = (props: Props) => {
+const Header = () => {
     return (
-        <header className="sticky top-0 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center p-2">
-            <motion.div
-                initial={{
-                    x: -500,
-                    opacity: 0,
-                    scale: 0.5,
-                }}
-                animate={{
-                    x: 0,
-                    opacity: 1,
-                    scale: 1,
-                }}
-                transition={{
-                    duration: 0.6,
-                }}
-                className="flex flex-row items-center"
-            >
-                {/* Social Icons */}
-                <SocialIcon
-                    url="https://github.com/owuraku-zenas"
-                    fgColor="gray"
+        <header className="sticky top-0 z-20 backdrop-blur-sm shadow-sm">
+            <div className="flex items-start justify-between max-w-7xl mx-auto xl:items-center p-2">
+                <motion.div
+                    initial={{
+                        x: -500,
+                        opacity: 0,
+                        scale: 0.5,
+                    }}
+                    animate={{
+                        x: 0,
+                        opacity: 1,
+                        scale: 1,
+                    }}
+                    transition={{
+                        duration: 0.6,
+                    }}
+                    className="flex flex-row items-center"
+                >
+                    {/* Social Icons */}
+                    <SocialIcon
+                        url="https://github.com/owuraku-zenas"
+                        fgColor="gray"
                     bgColor="transparent"
                 />
                 <SocialIcon
@@ -67,6 +66,7 @@ const Header = (props: Props) => {
                     Get In Touch
                 </p>
             </motion.div>
+            </div>
         </header>
     );
 };
